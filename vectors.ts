22bb30f617b4edb5d80b42286eeddced45d4acb0
Josiah Bruner <jsbruner@umich.edu>
2020-01-04 20:16:34 -0500

    Implement dot product and angle

--- vectors.ts
+++ vectors.ts
@@ -37,6 +37,48 @@ class Vector {
 
     return new Vector(newVector)
   }
+
+  public getMagnitude(): number {
+    let valBeforeSqrt = 0;
+
+    for (const val of this.vector) {
+      valBeforeSqrt += val * val;
+    }
+
+    return Math.sqrt(valBeforeSqrt)
+  }
+
+  public normalize(): Vector {
+    let magnitude = this.getMagnitude();
+
+    let newVector = []
+
+    for (const val of this.vector) {
+      newVector.push(val / magnitude);
+    }
+
+    return new Vector(newVector);
+  }
+
+  public dotProduct(otherVec: Vector): number {
+    let dotProductResult = 0; 
+
+    for (const [idx, val] of this.vector.entries()) {
+      dotProductResult += val * otherVec.getFromVector(idx);
+    }
+    
+    return dotProductResult; 
+  }
+
+  public getAngleBetween(otherVec: Vector, inDegrees: boolean): number {
+    let cosOfAngle = this.dotProduct(otherVec) / (this.getMagnitude() * otherVec.getMagnitude())
+
+    if (inDegrees === false) {
+      return Math.acos(cosOfAngle)
+    } else {
+      return Math.acos(cosOfAngle) * (180 / Math.PI)
+    }
+  }
 }
 
 // Main
@@ -48,3 +90,29 @@ console.log(new Vector([7.119, 8.215]).minus(new Vector([-8.223, 0.878])))
 
 console.log("Answer to 3")
 console.log(new Vector([1.671, -1.012, -0.318]).times(7.41))
+
+
+console.log("Answer to Magnitude 1")
+console.log(new Vector([-0.221, 7.437]).getMagnitude())
+
+console.log("Answer to Magnitude 2")
+console.log(new Vector([8.813, -1.331, -6.247]).getMagnitude())
+
+console.log("Answer to Normalize 1")
+console.log(new Vector([5.581, -2.136]).normalize())
+
+console.log("Answer to Normalize 2")
+console.log(new Vector([1.996, 3.108, -4.554]).normalize())
+
+
+console.log("Answer to dot product 1")
+console.log(new Vector([7.887, 4.138]).dotProduct(new Vector([-8.802, 6.776])))
+
+console.log("Answer to dot product 2")
+console.log(new Vector([-5.955, -4.904, -1.874]).dotProduct(new Vector([-4.496, -8.755, 7.103])))
+
+console.log("Answer to get angle 1")
+console.log(new Vector([3.183, -7.627]).getAngleBetween(new Vector([-2.668, 5.319]), false));
+
+console.log("Answer to get angle 2")
+console.log(new Vector([7.35, 0.221, 5.188]).getAngleBetween(new Vector([2.751, 8.259, 3.985]), true));
